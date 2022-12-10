3a09ceb78d290c0c4071dd3f888fb982d1983d08
Thomas <tseccombe@gmail.com>
2022-12-10 20:58:28 +0000

    Day 9 part 2 working

--- 2022/Day9/solve_parts.py
+++ 2022/Day9/solve_parts.py
@@ -18,6 +18,7 @@ class player():
         self.pos_y = pos_y
         self.prev_x = pos_x
         self.prev_y = pos_y
+        self.moved_dig = False
 
     def save_pos(self):
         self.prev_x = self.pos_x
@@ -34,11 +35,18 @@ class player():
         elif dir == 'R':
             self.pos_x += speed
 
+
 class chaser():
     def __init__(self, pos_x, pos_y):
         self.pos_x = pos_x
         self.pos_y = pos_y
+        self.prev_x = pos_x
+        self.prev_y = pos_y
         self.history = []
+
+    def save_pos(self):
+        self.prev_x = self.pos_x
+        self.prev_y = self.pos_y
 
     def chase_player(self, player):
         px = player.pos_x
@@ -52,27 +60,87 @@ class chaser():
             move_closer_y = True
 
         # a = 0
-        if  (move_closer_x and abs(py - self.pos_y) == 1)  or \
-            (move_closer_y and abs(px - self.pos_x) == 1) or \
-            (move_closer_x and move_closer_y)  :
+        if  (move_closer_x or move_closer_y):
             self.pos_x = player.prev_x
             self.pos_y = player.prev_y
 
-        elif move_closer_y:
-            diff = (py - self.pos_y) > 0# if pos_dif : player is in front, move forward
-            if diff:
-                self.pos_y += 1
-            else:
-                self.pos_y -= 1
-
-        elif move_closer_x:
-            diff = (px - self.pos_x) > 0
-            if diff:
-                self.pos_x += 1
-            else:
-                self.pos_x -= 1
-
         self.history.append(str(self.pos_x) + " " + str(self.pos_y))
+
+
+class Tail():
+    def __init__(self, pos_x, pos_y, last):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.last_tail = last
+        self.moved_dig = False
+        self.history = []
+
+    def chase_player(self, Head):
+        self.prev_x = self.pos_x
+        self.prev_y = self.pos_y
+
+        H_px = Head.pos_x
+        H_py = Head.pos_y
+
+        move_closer_x  = False
+        move_closer_y  = False
+        self.moved_dig = False # Reset for new move
+
+        if abs(H_px - self.pos_x) > 1:
+            move_closer_x = True
+        if abs(H_py - self.pos_y) > 1:
+            move_closer_y = True
+
+        # a = 0
+        if  (move_closer_x or move_closer_y) and not Head.moved_dig:
+            # if we are the first tail, need to check for diagonal move
+            if (abs(H_px - self.pos_x) > 0) and (abs(H_py - self.pos_y) > 0):
+                self.moved_dig = True
+            else:
+                self.moved_dig = False
+
+            # After checking, make the move
+            self.pos_x = Head.prev_x
+            self.pos_y = Head.prev_y
+
+
+        elif (move_closer_x or move_closer_y) and Head.moved_dig:
+            if (H_px - self.pos_x) == 0 or (H_py - self.pos_y) == 0:
+
+                if (H_px - self.pos_x) == 0:
+                    if (H_py - self.pos_y) > 0:
+                        self.pos_y += 1
+                    else:
+                        self.pos_y -= 1
+
+                if (H_py - self.pos_y) == 0:
+                    if (H_px - self.pos_x) > 0:
+                        self.pos_x += 1
+                    else:
+                        self.pos_x -= 1
+
+            else:
+                if (H_px - self.pos_x) > 0:
+                    self.pos_x += 1
+                    self.moved_dig = True
+                else:
+                    self.pos_x -= 1
+                    self.moved_dig = True
+
+                if (H_py - self.pos_y) > 0:
+                    self.pos_y += 1
+                    self.moved_dig = True
+                else:
+                    self.pos_y -= 1
+                    self.moved_dig = True
+
+        else:
+            pass # Dont move
+
+
+        if self.last_tail:
+            self.history.append(str(self.pos_x) + " " + str(self.pos_y))
+
 
 
 
@@ -97,9 +165,6 @@ def part_one(file_path):
             player1.save_pos()
             player1.move_player(move.dir)
             chaser1.chase_player(player1)
-            # print("player [x: " + str(player1.pos_x) + " - y: " + str(player1.pos_y) + " ]")
-            # print("chaser [x: " + str(chaser1.pos_x) + " - y: " + str(chaser1.pos_y) + " ]")
-            # print(" ")
 
     number_of_steps = len(chaser1.history)
     number_of_unique = len(set(chaser1.history))
@@ -107,10 +172,84 @@ def part_one(file_path):
 
     return number_of_unique
 
+def print_test_grid(head, tail_list, start):
+    test_grid = []
+    xM = 26
+    yM = 21
+
+    for y in range(yM):
+        row = []
+        for x in range(xM):
+            row.append(".")
+        test_grid.append(row)
+
+    test_grid[start[1]][start[0]] = 'S'
+
+    count = 9
+    for tail in tail_list:
+        test_grid[tail.pos_y][tail.pos_x] = str(count)
+        count -= 1
+
+    test_grid[head.pos_y][head.pos_x] = 'H'
+
+    for y in range(len(test_grid)-1, -1, -1):
+        for x in range(0, len(test_grid[0])):
+            if x != len(test_grid[0])-1:
+                print(test_grid[y][x], end =" ")
+            else:
+                print(test_grid[y][x])
+
+    print(" ")
+
+
 def part_two(file_path):
     """ Day n part two solution """
 
     lines = open_file(file_path)
 
-    return -1
+    moves = []
+    for line in lines:
+        dir, speed = line.split(' ')
+        new_move = step(dir, speed)
+        moves.append(new_move)
+
+    start = [0,0]
+    start = [11,5]
+
+    player1 = player(start[0], start[1])
+    tail1   = Tail(start[0], start[1], False)
+    tail2   = Tail(start[0], start[1], False)
+    tail3   = Tail(start[0], start[1], False)
+    tail4   = Tail(start[0], start[1], False)
+    tail5   = Tail(start[0], start[1], False)
+    tail6   = Tail(start[0], start[1], False)
+    tail7   = Tail(start[0], start[1], False)
+    tail8   = Tail(start[0], start[1], False)
+    tail9   = Tail(start[0], start[1], True)
+    # chaser1.history.append('00')
+
+    for move in moves:
+        for i in range(1, move.speed +1):
+            # tail_list = [tail9, tail8, tail7, tail6, tail5, tail4, tail3, tail2, tail1]
+            # print_test_grid(player1, tail_list, start)
+
+            player1.save_pos()
+            player1.move_player(move.dir)
+            tail1.chase_player(player1)
+            tail2.chase_player(tail1)
+            tail3.chase_player(tail2)
+            tail4.chase_player(tail3)
+            tail5.chase_player(tail4)
+            tail6.chase_player(tail5)
+            tail7.chase_player(tail6)
+            tail8.chase_player(tail7)
+            tail9.chase_player(tail8)
+
+
+
+    number_of_steps = len(tail9.history)
+    number_of_unique = len(set(tail9.history))
+
+
+    return number_of_unique
 
